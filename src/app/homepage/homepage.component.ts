a71e7b95d7217a112dcd4e246788ed8d957cca7d
kilonzijnr <vkilonzi05@gmail.com>
2021-08-23 12:15:05 +0300

    added constructors in the quote class

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
 
+import { Component ,OnInit } from '@angular/core';
+import { Quote } from '../quote'
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-
+  quotes:Quote[] = [
+  new Quote(1, 'Success is a do it yourself project', 'Derick Monu', 'Victor Kilonzi', new Date(2021,2,17) ,0 ,0),
+  new Quote(2, 'Dont loose track over a passing storm', 'Alex Karumbi', 'James Ngangi', new Date(2020,3,1) ,0 ,0),
+  ]
   constructor() { }
 
   ngOnInit(): void {
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,6 @@
 export class Quote {
+   showDetails:boolean
+   constructor(public id :number, public name :string, public Author :string, public User :string, public Date :Date, public upvote :number, public downvote :number){
+   this.showDetails=false
+}
 }
